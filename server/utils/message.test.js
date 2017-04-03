1f8bdfe7b79138c4b8ff948a8b203ed0422fd745
catfriend <eakari@live.com>
2017-04-03 14:12:41 -0700

    added geolocation via api

--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,6 +1,6 @@
 var expect = require('expect');
 
-var {generateMessage} = require('./message');
+var {generateMessage, generateLocationMessage} = require('./message');
 
 describe('generateMessage', () => {
   it('should generate correct message object', () => {
@@ -9,9 +9,19 @@ describe('generateMessage', () => {
     var message = generateMessage(from, text);
 
     expect(message.createdAt).toBeA('number');
-    expect(message).toInclude({
-      from,
-      text,
-    });
+    expect(message).toInclude({from, text});
+  });
+});
+
+describe('generateLocationMessage', () => {
+  it('should generate correct location object', () => {
+    var from = 'Patti';
+    var latitude = '12';
+    var longitude = '14';
+    var url = 'https://www.google.com/maps?q=12,14';
+    var message = generateLocationMessage(from, latitude, longitude);
+
+    expect(message.createdAt).toBeA('number');
+    expect(message).toInclude({from, url});
   });
 });
